fcdb253dcb471aad61bb4c83bb53e35085bbac4a
Alois Leitner <alois.leitner@amv-networks.com>
2017-03-30 17:30:51 +0200

    implement FeignClientDemoApplicationTests

--- amv-trafficsoft-rest-client/src/test/java/org/amv/trafficsoft/rest/client/FeignClientDemoApplicationTests.java
+++ amv-trafficsoft-rest-client/src/test/java/org/amv/trafficsoft/rest/client/FeignClientDemoApplicationTests.java
@@ -1,17 +1,33 @@
 package org.amv.trafficsoft.rest.client;
 
+import org.amv.trafficsoft.rest.client.asgregister.AsgRegisterClient;
+import org.amv.trafficsoft.rest.client.xfcd.XfcdClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class FeignClientDemoApplicationTests {
+
+    @Autowired
+    private Optional<AsgRegisterClient> asgRegisterClient = Optional.empty();
+
+    @Autowired
+    private Optional<XfcdClient> xfcdClient = Optional.empty();
 
     @Test
     public void contextLoads() {
-
+        assertThat(asgRegisterClient.isPresent(), is(true));
+        assertThat(xfcdClient.isPresent(), is(true));
     }
 
 }
--- amv-trafficsoft-rest-client/src/test/java/org/amv/trafficsoft/rest/client/FeignClientDemoTestConfig.java
+++ amv-trafficsoft-rest-client/src/test/java/org/amv/trafficsoft/rest/client/FeignClientDemoTestConfig.java
@@ -1,15 +1,46 @@
 package org.amv.trafficsoft.rest.client;
 
-import feign.Target;
-import feign.mock.MockClient;
-import feign.mock.MockTarget;
-import org.amv.trafficsoft.rest.client.ClientConfig.ConfigurableClientConfig;
 import org.amv.trafficsoft.rest.client.asgregister.AsgRegisterClient;
 import org.amv.trafficsoft.rest.client.xfcd.XfcdClient;
+import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignClientDemoTestConfig {
 
+    @Bean
+    public ClientConfig.BasicAuth basicAuth() {
+        return new ClientConfig.BasicAuth() {
+            @Override
+            public String username() {
+                return RandomStringUtils.randomAscii(10);
+            }
+
+            @Override
+            public String password() {
+                return RandomStringUtils.randomAscii(120);
+            }
+        };
+    }
+
+    @Bean
+    public ClientConfig<AsgRegisterClient> asgRegisterClientConfig() {
+        return TrafficsoftClients.config(AsgRegisterClient.class, "https://example.com", basicAuth());
+    }
+
+    @Bean
+    public AsgRegisterClient asgRegisterClient() {
+        return TrafficsoftClients.asgRegister(asgRegisterClientConfig());
+    }
+
+    @Bean
+    public ClientConfig<XfcdClient> xfcdClientClientConfig() {
+        return TrafficsoftClients.config(XfcdClient.class, "https://example.com", basicAuth());
+    }
+
+    @Bean
+    public XfcdClient xfcdClient() {
+        return TrafficsoftClients.xfcd(xfcdClientClientConfig());
+    }
 }
