fb6d7ad699a3616a7c4447b56d835a1dd46ef801
amvnetworks <development@amv-networks.com>
2017-06-23 12:47:08 +0200

    remove improper example value in VehicleWhitelistRestDto

--- amv-trafficsoft-rest-model/src/main/java/org/amv/trafficsoft/rest/carsharing/whitelist/model/VehicleWhitelistRestDto.java
+++ amv-trafficsoft-rest-model/src/main/java/org/amv/trafficsoft/rest/carsharing/whitelist/model/VehicleWhitelistRestDto.java
@@ -21,7 +21,7 @@ public class VehicleWhitelistRestDto {
     @ApiModelProperty(value = "Id of a vehicle", required = true)
     private long vehicleId;
 
-    @ApiModelProperty(value = "A list of driver tag ids", allowEmptyValue = true, example = "[545810DB,14C76FDA,040D2332FF2981]")
+    @ApiModelProperty(value = "A list of driver tag ids", allowEmptyValue = true)
     @Singular(value = "addWhitelist")
     private List<String> whitelist;
 }
